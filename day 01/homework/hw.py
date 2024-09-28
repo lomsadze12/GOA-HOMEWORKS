0bc4a7dc841758d788c29c12d476c685fa2705dd
lomsadze12 <lomsadzegiorgi2323@gmail.com>
2024-09-28 21:25:27 +0400

    first commit

---
+++ day 01/homework/hw.py
@@ -0,0 +1,36 @@
+name= "ნერსე"
+
+
+surname= "ლომასძე"
+
+print(name)
+
+
+
+
+name ="ნერსე"
+age =  10
+heigth = 138
+
+
+knows_programing = True
+is_ugly =False
+
+
+
+
+print(name +""+ surname)
+
+
+
+
+print(name + " " +  str(age))
+
+has_pet=True
+
+lives_in_tbilisi=True
+
+print(has_pet + " " + lives_in_tbilisi + " " + is_ugly + " " + knows_programing)
+
+
+print( name + surname )

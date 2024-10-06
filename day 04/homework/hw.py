563ace7cf9a9383d9d73ea43aa901c3c6a0b5b2b
lomsadze12 <lomsadzegiorgi2323@gmail.com>
2024-10-07 00:01:19 +0400

    commit

---
+++ day 04/homework/hw.py
@@ -0,0 +1,150 @@
+# hw 01
+# +
+num1 = 4 
+num2 = 5
+print(num1 + num2)
+
+num3 = 7
+num4 = 1
+print(num3 + num4)
+
+num5 = 45
+num6 = 76
+print(num6 + num5)
+
+num7 = 5
+num8 = 7
+print(num7 + num8)
+
+num9 = 90
+num10 = 100
+print(num10 + num9)
+
+
+# -
+num1 = 435
+num2 = 67
+print(num1 - num2)
+
+num3 = 234
+num4 = 34
+print(num3 - num4)
+
+num5 = 12
+num6 = 76
+print(num6 - num5)
+
+num7 = 789
+num8 = 890
+print(num7 - num8)
+
+num9 = 9
+num10 = 100
+print(num10 - num9)
+
+
+# *
+num1 = 3
+num2 = 6
+print(num1 * num2)
+
+num3 = 2
+num4 = 3
+print(num3 * num4)
+
+num5 = 12
+num6 = 345
+print(num6 * num5)
+
+num7 = 7
+num8 = 8
+print(num7 * num8)
+
+num9 = 3
+num10 = 4
+print(num10 * num9)
+
+
+# /
+num1 = 2543
+num2 = 34
+print(num1 / num2)
+
+num3 = 123
+num4 = 4
+print(num3 / num4)
+
+num5 = 345
+num6 = 345
+print(num6 / num5)
+
+num7 = 14
+num8 = 7
+print(num7 / num8)
+
+num9 = 67
+num10 = 34
+print(num10 / num9)
+
+# //
+num1 = 350
+num2 = 2
+print(num1 // num2)
+
+num3 = 266
+num4 = 3
+print(num3 // num4)
+
+num5 = 750
+num6 = 3
+print(num6 // num5)
+
+num7 = 450
+num8 = 3
+print(num7 // num8)
+
+num9 = 230
+num10 = 1
+print(num10 // num9)
+
+
+# %
+num1 = 350
+num2 = 4
+print(num1 % num2)
+
+num3 = 247
+num4 = 3
+print(num3 % num4)
+
+num5 = 230
+num6 = 5
+print(num6 % num5)
+
+num7 = 47
+num8 = 2
+print(num7 % num8)
+
+num9 = 100
+num10 = 3
+print(num10 % num9)
+
+
+# hw 02
+age = 11
+age_after = 16
+print(age, age_after)
+
+
+# hw 03
+father = 44
+mother = 47
+brother = 15
+me = 11
+print(father + mother + brother + me)
+
+
+# hw 04
+name = "Nerse"
+name = "Data"
+print(name)
---
+++ day 05/classwork/cw.py
@@ -0,0 +1,5 @@
+# cw 01
+name = input("enter your name: ")
+lastname = input("enter your lastname: ")
+age = input("enter your age: ")
+print("your name is " + name + ", your lastname is " + lastname + ", your age is " + age + ".")

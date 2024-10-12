f9cdde044a5947d1933e8aac109a7f2c31bb292d
lomsadze12 <lomsadzegiorgi2323@gmail.com>
2024-10-12 20:13:56 +0400

    commit

--- day 09/classwork/cw.py
+++ day 09/classwork/cw.py
@@ -1,2 +1,12 @@
-input (age = 10)
+# cw 01
+age = int(input("enter your age: "))
+print(age // 5)
 
+# cw 02
+name = input("enter your name: ")
+num = 2
+print(name * num)
+
+# cw 03
+age = int(input("enter your age: "))
+print(type(age))
---
+++ day 09/homework/hw.py
@@ -0,0 +1,17 @@
+# hw 01
+age = int(input("enter your age: "))
+print(age // 10)
+
+# hw 02
+name = input("enter your name: ")
+print(name == "nerse")
+
+# hw 03
+num1 = int(input("enter number: "))
+num2 = int(input("enter number: "))
+print(num1 % num2)
+
+# hw 04
+# შედეგად
+age = int(input("enter your age: "))
+print(age % 2 == 0)
